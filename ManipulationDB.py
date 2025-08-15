90a2a2a75f588acaa4b65043a7671586a3dcc55d
Nahfulg <fufunahimana@gmail.com>
2025-08-15 10:28:06 +0200

    'update'

--- ManipulationDB.py
+++ ManipulationDB.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('TestDatabase.db')
+conn = sqlite3.connect('ManipulationDB.db')
 cursor = conn.cursor()
 print("Connexion à la base de données établie avec succès.")
 
@@ -16,14 +16,14 @@ conn.commit()
 print("Table 'utilisateurs' créée avec succès.")
 
 # Données à insérer (avec INSERT OR IGNORE)
-donnees_utilisateur = ('Nahimana fulgence', 'funfulgence@gmail.com')
+donnees_utilisateur = ('Nsengiyumva seraphine', 'nsengiyumvaseraphine123@gmail.com')
 cursor.execute("INSERT OR IGNORE INTO utilisateurs (nom, email) VALUES (?, ?)", donnees_utilisateur)
 conn.commit()
 print("Utilisateur inséré (ou ignoré si déjà existant).")
 
 # Insertion multiple avec protection contre doublons
 utilisateurs = [
-    ('séraphine NSENGIYUMVA', 'nsengiyumvaseraphine123@gmail.com'),
+    ('Seraphine NSENGIYUMVA', 'nsengiyumvaseraphine123@gmail.com'),
     ('Albert Mukunzi', 'albert.mukunzi@gmail.com')
 ]
 cursor.executemany("INSERT OR IGNORE INTO utilisateurs (nom, email) VALUES (?, ?)", utilisateurs)
@@ -41,7 +41,7 @@ for utilisateur in resultats:
     print(utilisateur) 
 
 # Sélectionner un utilisateur en particulier 
-nom_recherche = 'Nahimana fulgence' 
+nom_recherche = 'Nsengiyumva seraphine' 
 cursor.execute("SELECT email FROM utilisateurs WHERE nom=?", 
 (nom_recherche,)) 
 # Récupérer le premier résultat (ou None si rien n'est trouvé) 
@@ -49,8 +49,8 @@ email_trouve = cursor.fetchone()
 print(f"L'e-mail de {nom_recherche} est : {email_trouve[0]}") 
 
 # Mettre à jour l'e-mail de 'Jean Claude Kabayabaya'' 
-nouveau_mail = 'fufunahimana@gmail.com' 
-nom_a_modifier = 'nahimana fulgence' 
+nouveau_mail = 'nsengiyumvaseraphine123@gmail.com' 
+nom_a_modifier = 'Nsengiyumva seraphine' 
 cursor.execute("UPDATE utilisateurs SET email=? WHERE nom=?", 
 (nouveau_mail, nom_a_modifier)) 
 conn.commit() 
